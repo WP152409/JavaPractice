bb713ab4f879de9484bae9856086cd984ef19af4
ChalkPE <chalk@chalk.pe>
2017-03-14 15:29:48 +0900

    실습 2

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * org.dimigo.basic
+ *  \_ Operator
+ *
+ * 연산자에 대해 알아봅시다
+ *
+ * @author chalk
+ * @version 1.0.0
+ * @since Mar 14, 2017
+ */
+public class Operator {
+	public static void main(String[] args) {
+		long salary = 1700000L, employees= 3L, stores = 1500L;
+
+		System.out.printf(
+				"<< 디미베네 연간 인건비 >>%n월 평균 급여 : %,d원%n점포 내 직원 수 : %d명%n"
+				+ "점포 수 : %,d개%n%n연간 인건비 : %,d원%n", salary, employees, stores,
+				12L * salary * employees * stores);
+	}
+}
