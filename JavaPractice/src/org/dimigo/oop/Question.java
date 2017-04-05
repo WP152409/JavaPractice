827235fe4f479702aa2dc413cc3143bbb92aae8c
ChalkPE <chalk@chalk.pe>
2017-04-05 10:18:26 +0900

    Use foreach

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -18,14 +18,14 @@ public class Question {
 		String[][] list = {{"가수는", "Zedd"}, {"배우는", "undefined"}, {"과목은", "물리"}};
 		
 		for(int i = 0; i < list.length; i++){
-			System.out.printf("%d. 가장 좋아하는 %s?%n", i + 1, list[0][i]);
-			System.out.println(list[1][i].equals(scanner.nextLine()) ? "정답입니다" : "틀렸습니다");
+			System.out.printf("%d. 가장 좋아하는 %s?%n", i + 1, list[i][0]);
+			System.out.println(list[i][1].equals(scanner.nextLine()) ? "정답입니다" : "틀렸습니다");
 		}
 		
 		StringBuilder b = new StringBuilder();
-		for(int i = 0; i < list.length; i++) b
-				.append("가장 좋아하는 ").append(list[0][i]).append("? ")
-				.append(list[1][i]).append("입니다.").append(System.lineSeparator());
+		for(String[] item: list) b
+				.append("가장 좋아하는 ").append(item[0]).append("? ")
+				.append(item[1]).append("입니다.").append(System.lineSeparator());
 		
 		System.out.printf("<< 결과 출력 >>%n%s%n", b.toString());
 		scanner.close();
