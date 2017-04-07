284fdb09a83407530eb1e43406c7296c10bd290a
ChalkPE <chalk@chalk.pe>
2017-04-07 11:49:43 +0900

    Stream!

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -1,5 +1,7 @@
 package org.dimigo.oop;
 
+import java.util.Arrays;
+
 /**
  * org.dimigo.oop
  *  \_ SnackTest
@@ -12,18 +14,14 @@ package org.dimigo.oop;
  */
 public class SnackTest {
 	public static void main(String[] args) {
-		Snack[] snacks = new Snack[] {
-			new Snack("새우깡", "농심", 1100, 2),
-			new Snack("콘칲", "크라운", 1200, 1),
-			new Snack("허니버터칩", "해태", 1500, 4)
-		};
-		
-		int sum = 0;
-		for(Snack snack: snacks) {
-			sum += snack.calcPrice();
-			System.out.println(snack.toString());
-		}
-		
-		System.out.printf("총 구매 금액 : %,d원%n", sum);
+		System.out.printf(
+			"총 구매 금액 : %,d원%n",
+			Arrays.stream(new Snack[] {
+				new Snack("새우깡", "농심", 1100, 2),
+				new Snack("콘칲", "크라운", 1200, 1),
+				new Snack("허니버터칩", "해태", 1500, 4)
+			})
+			.peek(System.out::println)
+			.mapToInt(Snack::calcPrice).sum());
 	}
 }
